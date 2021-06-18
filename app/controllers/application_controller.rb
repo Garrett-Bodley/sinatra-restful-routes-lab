fbf5c215af75456140e2ac11db3d08b8e061f7c2
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:50:45 -0400

    Archive Flatiron code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new_recipe
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = current_recipe
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = current_recipe
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    current_recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{current_recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    current_recipe.destroy
+    redirect '/recipes'
+  end
+
+  helpers do
+    def current_recipe
+      Recipe.find_by(id: params[:id])
+    end
+  end
 
 end
